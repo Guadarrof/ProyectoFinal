b98143fa250e558537eb5ede54e4f0729f989aeb
“Guada” <guadalupearroferia@gmail.com>
2024-01-15 16:02:26 +1000

    created js for validations on upload form

--- JS/index.js
+++ JS/index.js
@@ -1,41 +1,12 @@
-let formName= document.getElementById('name');
-let formPrice=document.getElementById('price');
-let formStock=document.getElementById('stock');
-let formBrand=document.getElementById('brand');
-let formCategory=document.getElementById('category');
-let formShortDescription=document.getElementById('shortDescription');
-let formLongDescription=document.getElementById('longDescription');
-let formDeliveryChk=document.getElementById('delivery');
-let formStartAge=document.getElementById('startAge');
-let formEndAge=document.getElementById('endAge');
-let formPicture=document.getElementById('picture');
-let inputs=document.getElementsByTagName('input');
-
-for (let i = 0; i < inputs.length; i++) {
-   console.log(inputs[i].type); 
-}
-
-function retrieveText(){
-    
-}
-
-
-
-// let priceRegEx= new RegExp ('^\$\d{1,3}(,\d{3})*(\.\d{2})?$');
-
-
-
-// function validateNums(){
-//     for (let i = 0; i < inputs.length; i++) {
-//         if (inputs[i].type=="number"){
-//             if (inputs[i].id="price") {
-//                 if(!priceRegEx){
-//                     return "El precio no puede contener caracteres alfanumericos";
-//                 }
-                
-//             }
-
-//         }
-//     }
-
-// }
+let formName= document.getElementById('nameForm');
+let formPrice=document.getElementById('priceForm');
+let formStock=document.getElementById('stockForm');
+let formBrand=document.getElementById('brandForm');
+let formCategory=document.getElementById('categoryForm');
+let formShortDescription=document.getElementById('shortDescriptionForm');
+let formLongDescription=document.getElementById('longDescriptionForm');
+let formDeliveryChk=document.getElementById('deliveryForm');
+let formStartAge=document.getElementById('startAgeForm');
+let formEndAge=document.getElementById('endAgeForm');
+let formPicture=document.getElementById('pictureForm');
+let inputList=document.getElementsByTagName('input');
